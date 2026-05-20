9601b78e83eb834cdcd7b5eb619a140ba48b76a4
tsiksdev-sketch <tsiksdev@gmail.com>
2026-05-20 14:29:47 +0200

    #

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,3 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Africarbontraining'
-export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'join us in driving a resilient and low-carbon future for Africa.   '
-export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A Carbon Reduction Institute websites built with Next.js' 
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'GrowAtree'
+export const APP_SLOGAN = process.env.NEXT_PUBLIC_APP_SLOGAN || 'Plant Today. Grow Tomorrow.  '
+export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'A Grow a tree foundation website built with Next.js' 
